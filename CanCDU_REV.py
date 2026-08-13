837a52df98edabf51fe858fc9477e095bce6278e
choongmin <likeaz6@gmail.com>
2026-08-13 14:11:56 +0900

    1. 모드동작 분기 개선(LEGACY/HMI, LT/DT/OT) 2. Test모드, KF_update_flag 등 상태 숫자대신 'TEXT'로 명시화

--- CanCDU_REV.py
+++ CanCDU_REV.py
@@ -41,9 +41,30 @@ BITRATES = {
     "10K": canlib.Bitrate.BITRATE_10K,
 }
 
+TEST_MODE = {
+    8: "LT",
+    9: "DT",
+    10: "OT",
+}
+
+BGZ_UPDATE_MODE = {
+    0: "LEGACY",
+    1: "HMI",
+}
+
+KF_UPDATE_FLAG = {
+    0: "PURE",
+    2: "ZUPT",
+    3: "AID",
+    4: "AID",
+    5: "AID-HMI"
+}
+
 # Edit payload items only here. type is Python struct format: f=float32, i=int32, x=pad byte.
 PAYLOAD_FIELDS = [
     # name                 type scale               csv_fmt
+    ("test_mode",          "i", 1,                  "d"),
+    ("bgz_update_mode",    "i", 1,                  "d"),
     ("time_gps_hhmmss",    "i", 1,                  "d"),
     ("timestamp_sec",      "f", 1.0,                ".3f"),
     ("kf_update",          "i", 1,                  "d"),
@@ -122,10 +143,10 @@ def tx_loop(ch, can_lock, stop_event):
             tx_payload[0] = cnt  # reserved
             tx_payload[1] = 0x01
             # tx_payload[2] = 0x00 # reserved
-            tx_payload[3] = 0x00 #0x7F
-            tx_payload[4] = 0x00 #0xFF
+            tx_payload[3] = 0x00 
+            tx_payload[4] = 0xF0 #0x09 
 
-            wheel_angle_adc = 479 # 479(0.378337) # 508(4.7536845)
+            wheel_angle_adc = 559 # 515 # 559(4.501164) - MT9
             tx_payload[5:7] = wheel_angle_adc.to_bytes(2, byteorder="big", signed=True)
 
             #같은 CAN channel을 RX 루프의 ch.read()와 동시에 접근하지 않도록 lock하고, 그 다음 송신함
@@ -145,7 +166,7 @@ def tx_loop(ch, can_lock, stop_event):
 
 now = time.localtime()
 output_path = (
-    f"LT_IEKF_REV_{now.tm_year % 100:02d}{now.tm_mon:02d}{now.tm_mday:02d}_"
+    f"DT_IEKF_REV_{now.tm_year % 100:02d}{now.tm_mon:02d}{now.tm_mday:02d}_"
     f"{now.tm_hour:02d}{now.tm_min:02d}{now.tm_sec:02d}.txt"
 )
 
@@ -247,9 +268,12 @@ with open(output_path, mode="w", newline="", encoding="utf-8") as output_file:
                                 if sec != old_sec:
                                     get = values.get
                                     print(
-                                        f"[{get('time_gps_hhmmss', 0)}] "
+                                        f"[{TEST_MODE.get(get('test_mode', 0), 'Unknown')}] "
+                                        f"[{BGZ_UPDATE_MODE.get(get('bgz_update_mode', 0), 'Unknown')}] "
+                                        f"[{get('time_gps_hhmmss', 0):06d}] "
                                         f"[{get('timestamp_sec', 0.0):.1f}] "
-                                        f"[{int(get('kf_update', 0))}] "
+                                        # f"[{int(get('kf_update', 0))}] "
+                                        f"[{KF_UPDATE_FLAG.get(get('kf_update', 0), 'Unknown')}] "
                                         # f"[s0 dt] {get('sample0_dt_sec', 0.0):.6f}s, "
                                         # f"[s1 dt] {get('sample1_dt_sec', 0.0):.6f}s, "
                                         # f"[s0 gyro] {get('sample0_gyro_x_dps', 0.0):.3f}, "
@@ -271,9 +295,10 @@ with open(output_path, mode="w", newline="", encoding="utf-8") as output_file:
                                         f"[g-pos] {get('gps_lat_deg', 0.0):.7f}, "
                                         f"{get('gps_lon_deg', 0.0):.7f}, "
                                         f"{get('gps_hgt_m', 0.0):.2f}, "
-                                        # f"[hmi] {get('hmi_acu_Lv', 0)}, "
-                                        # f"{get('hmi_wheel_speed_kph', 0.0):.6f}, "
-                                        # f"{get('hmi_wheel_angle_deg', 0.0):.6f} "
+                                        f"[g-speed] {get('ground_speed_mps', 0.0) * 3.6:.2f}, "
+                                        f"[hmi] {get('hmi_acu_Lv', 0)}, "
+                                        f"{get('hmi_wheel_speed_kph', 0.0):.2f}, "
+                                        f"{get('hmi_wheel_angle_deg', 0.0):.6f} "
                                         ,
                                         flush=True,
                                     )
